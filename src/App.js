2cc053af181d5f4bbf621b1a1db2658be006fca3
Varshini_J <varshinijayabalan25@gmail.com>
2023-11-24 17:12:02 +0530

    example for usestate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+// function countInitial() {
+//   return 4
+// }
 function App() {
+  // const [count,setcount]=useState(()=>  //usestate using function
+  // {                                     //it call the function each time
+  //   return 4
+  // })
+  //const [count,setcount]=useState(()=>countInitial())  //usestate using function
+  const [count,setcount]=useState(4)  //usestate without function it call only at starting
+  // const [count,setcount]=useState({a : 4, b: 'black'});//more than one value
+  // const a=count.a;
+  // const b=count.b;
+  function decrementCount(){
+     setcount(count-1)
+    // setcount(count-1)//no changes if it is twotimes in this form
+    // setcount(preva=>{//it show black at initial and not show after changes 
+    //   return {a: preva.a -1}
+    // });
+    // setcount(preva=>{//it show even we change the value
+    //   return {...preva,a: preva.a -1}
+    // });
+  }
+  function incrementCount(){
+    setcount(prevcount=>prevcount+1)
+    setcount(prevcount=>prevcount+1)//increased by two if it is in this form
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <button onClick={decrementCount}>-</button>
+    {/* <span>{a}</span>
+    <span>{b}</span> */}
+    <span>{count}</span> 
+     <button onClick={incrementCount}>+</button>
+    </>
   );
 }
 
